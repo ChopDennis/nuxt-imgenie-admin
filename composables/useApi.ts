c01e8fab9c30064c2dcb5811b3fe6456dd186e78
Dennis Huang <dennis.huang@etanits.com>
2023-11-06 16:57:34 +0800

    feat: 移除舊專案邏輯以及程式 [IM-307]

--- composables/useApi.ts
+++ composables/useApi.ts
@@ -1,66 +1,30 @@
 // TODO: type Methods = "GET" | "POST" | "DELETE" | "PUT"
 
 enum Api {
-  datamart = "/api/datamart",
-  chatsql = "/api/chatsql",
-  note = "/api/note",
+  dbConnection = "/api/dbConnection",
 }
 
-export const useDataMartApi = async (
+export const useDbConnectionApi = async (
   url: string,
   params?: any,
 ): Promise<ApiResponse> => {
   let result: ApiResponse = {};
 
-  const { data, error } = await useFetch(`${Api.datamart}/${url}`, {
+  const { data, error } = await useFetch(`${Api.dbConnection}/${url}`, {
     method: "post",
     body: {
       ...params,
     },
+    lazy: true,
+    deep: false,
   });
   if (error.value) {
     console.error("useFetch 錯誤訊息: ", error.value.data); // eslint-disable-line no-console
     result = error.value.data.data as ApiResponse;
   } else {
-    console.log("useFetch 正確訊息: ", data.value); // eslint-disable-line no-console
-    result = data.value as ApiResponse;
-  }
-  return result;
-};
-
-export const useChatSqlApi = async (
-  url: string,
-  params?: any,
-): Promise<ApiResponse> => {
-  let result: ApiResponse = {};
-  const { data, error } = await useFetch(`${Api.chatsql}/${url}`, {
-    method: "post",
-    body: params,
-  });
-  if (error.value) {
-    console.error("useFetch 錯誤訊息: ", error.value.data); // eslint-disable-line no-console
-    result = error.value.data.data as ApiResponse;
-  } else {
-    console.log("useFetch 正確訊息: ", data.value); // eslint-disable-line no-console
-    result = data.value as ApiResponse;
-  }
-  return result;
-};
-
-export const useNoteApi = async (
-  url: string,
-  params?: any,
-): Promise<ApiResponse> => {
-  let result: ApiResponse = {};
-  const { data, error } = await useFetch(`${Api.note}/${url}`, {
-    method: "post",
-    body: params,
-  });
-  if (error.value) {
-    console.error("useFetch 錯誤訊息: ", error.value.data); // eslint-disable-line no-console
-    result = error.value.data.data as ApiResponse;
-  } else {
-    console.log("useFetch 正確訊息: ", data.value); // eslint-disable-line no-console
+    console.log(`useFetch ${Api.dbConnection}/${url} - ${formatCurrentTime()}`); // eslint-disable-line no-console
+    const table = data.value as any;
+    console.table(table.data); // eslint-disable-line no-console
     result = data.value as ApiResponse;
   }
   return result;
