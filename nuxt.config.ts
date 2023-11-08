5ea7cc005c12f148f4c404f847ebec45df431eb2
Dennis Huang <dennis.huang@etanits.com>
2023-11-08 15:04:19 +0800

    chore: 修正 Vue router 在 HMR 時，page transition 的錯誤 - 參考資料: https://github.com/vuejs/router/issues/341 [IM-307]

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   app: {
-    pageTransition: { name: "page", mode: "out-in" },
+    pageTransition:
+      process.env.NODE_ENV === "production"
+        ? {
+            name: "page",
+            mode: "out-in",
+          }
+        : false,
   },
   css: ["@/assets/css/main.css"],
   devtools: {
